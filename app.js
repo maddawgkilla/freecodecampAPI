c04badcff697ac6f38913d322d1ae45d5292f120
Sami Tariq <maddawgkilla@gmail.com>
2019-02-03 19:15:24 +0500

    Added the whoami api route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.enable('trust proxy');
+
 app.get("/api/timestamp/:date_string", (req,res) => {
     const input = req.params.date_string;
     const check = new Date(input);
@@ -10,6 +12,14 @@ app.get("/api/timestamp/:date_string", (req,res) => {
     } else {
         res.json({ unix: check.getTime(), utc: check.toUTCString() });
     }
+});
+
+app.get('/api/whoami', (req, res) => {
+    const data = req;
+    // console.log(data);
+    const result = { ipaddress: data.ip, language: data.headers["accept-language"], software: data.headers["user-agent"] };
+    console.log(result);
+    res.json(result);
 });
 
 app.listen(3000, () => {
